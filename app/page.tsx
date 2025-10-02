ef8990f6524698dc9bf2c92a37ad92574644d605
Ayush Routray <222822128+ayushroutray5999-wq@users.noreply.github.com>
2025-10-02 13:42:50 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <><div>
+      <h1 className="text-3xl font-bold text-center text-rose-500">Hello, Ayush</h1>
+      <Button>Click me</Button>
+      </div></>
+  );
+}
